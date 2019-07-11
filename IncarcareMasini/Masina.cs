b10d9341f0c508c651f7c6a5876ed150e1c3a6ae
Usurelu Daniel Constantin <usureludanielconstantin@yahoo.com>
2019-07-11 19:53:04 +0200

    first commit

---
+++ IncarcareMasini/Masina.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncarcareMasini
+{
+    public class Masina
+    {
+        const int kwPeOra = 11;
+        public string name { get; set; }
+        public int availableBattery { get; set; } // baterie disponibila
+        public int capacitateTotala  {get; set; } // capacitatea total a masini in kW
+        public int procentFinal  {get; set; } // procentual cat sa fie incarcata masina
+        public DateTime oraAlimentare {get; set; } // ora sosirii masinii
+        public int durataIncarcarii { get; set; } // in minute
+        public int nrOreAsteptare { get; set; } // nr de ore dupa care se va intoarce proprietarul
+        public int nrMinimDeAsteptare { get; set; } // nr Minim dintre nr de Ore de asteptare si numarul de Ore de incarcare
+
+
+        public Masina(string name, int availableBattery, int capacitateTotala, int procentFinal, DateTime oraAlimentare, int nrOreAsteptare)
+        {
+            this.name = name;
+            this.availableBattery = availableBattery;
+            this.capacitateTotala = capacitateTotala;
+            this.procentFinal = procentFinal;
+            this.oraAlimentare = oraAlimentare;
+            this.nrOreAsteptare = nrOreAsteptare;
+        }
+
+        public void calculeazaDurataIncarcarii()
+        {
+            int procentNecesar = this.procentFinal - this.availableBattery;
+            float kwNecesari = (procentNecesar * capacitateTotala) / 100;
+            this.durataIncarcarii = (int) Math.Round(((capacitateTotala / kwNecesari) * 60));
+        }
+
+        public void ordonareDupaTimp()
+        {
+            this.nrMinimDeAsteptare = nrOreAsteptare * 60 - durataIncarcarii;
+        }
+
+    }
+}
---
+++ IncarcareMasini/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncarcareMasini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Usurelu\source\repos\IncarcareMasini\IncarcareMasini\MasiniDeIncarcat.txt");
+
+            System.Console.WriteLine("Contents of  = MasiniDeIncarcat.txt");
+            int tensiuneTotala = Int32.Parse(lines[0]);
+
+            List<Masina> listaMasini = new List<Masina>();
+            string filename = @"C:\Users\Usurelu\source\repos\IncarcareMasini\IncarcareMasini\MasiniDeIncarcat.txt";
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            StreamReader rdr = new StreamReader(fs);
+            
+                rdr.ReadLine();
+
+                while (!rdr.EndOfStream)
+                {
+                    string[] linia = rdr.ReadLine().Split();
+                    listaMasini.Add( new Masina(linia[0], Int32.Parse(linia[1]), Int32.Parse(linia[2]), Int32.Parse(linia[3]), DateTime.Parse(linia[4] + ' ' + linia[5]), Int32.Parse(linia[6])));
+                    //.WriteLine("\t" + linia[4]);
+                }
+
+
+            foreach (Masina car in listaMasini)
+            {
+                car.calculeazaDurataIncarcarii();
+                car.ordonareDupaTimp();
+                Console.WriteLine("\t" + car.durataIncarcarii);
+            }
+
+            List<Masina> SortedList = listaMasini.OrderBy(o => o.nrMinimDeAsteptare).ToList();
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"C:\Users\Usurelu\source\repos\IncarcareMasini\IncarcareMasini\WriteLines2.txt"))
+            {
+                foreach (Masina m in SortedList)
+                {
+                    // If the line doesn't contain the word 'Second', write the line to the file.
+                   
+                        file.WriteLine(m.name, m.oraAlimentare);
+
+                }
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+
+        
+    }
+}
